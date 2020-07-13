7b95069a96f9090184dd059e7bf487d5598679db
Selina Kim <selinakim@fb.com>
2020-07-13 13:40:30 -0700

    Set up LoginView model and observers, as well as sign in button onClickListner

--- app/src/main/java/com/example/nextstreet/login/LoginActivity.java
+++ app/src/main/java/com/example/nextstreet/login/LoginActivity.java
@@ -1,10 +1,15 @@
 package com.example.nextstreet.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.nextstreet.MainActivity;
@@ -24,5 +29,44 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        if (ParseUser.getCurrentUser() != null) {
+            goActivity(MainActivity.class);
+        }
+
+        loginViewModel =
+                ViewModelProviders.of(this).get(LoginViewModel.class);
+
+        loginViewModel.getUsername().observe(this, new LoginActivity.textObserver(binding.etUsername));
+        loginViewModel.getPassword().observe(this, new LoginActivity.textObserver(binding.etPassword));
+
+        binding.btnSignup.setOnClickListener(new signupOnClickListener());
+    }
+
+    private class textObserver implements Observer<String> {
+        TextView tv;
+
+        private textObserver(TextView tv) {
+            this.tv = tv;
+        }
+
+        @Override
+        public void onChanged(@Nullable String s) {
+            tv.setText(s);
+        }
+    }
+
+    private class signupOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Log.i(TAG, "signupOnClickListener onClick: ");
+            goActivity(SignupActivity.class);
+        }
+    }
+    
+    private void goActivity(Class c) {
+        Intent i = new Intent(this, c);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/nextstreet/login/LoginViewModel.java
+++ app/src/main/java/com/example/nextstreet/login/LoginViewModel.java
@@ -1,6 +1,24 @@
 package com.example.nextstreet.login;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class LoginViewModel extends ViewModel {
+
+    private MutableLiveData<String> username;
+    private MutableLiveData<String> password;
+
+    public LoginViewModel() {
+        username = new MutableLiveData<>();
+        password = new MutableLiveData<>();
+    }
+
+    public LiveData<String> getUsername() {
+        return username;
+    }
+
+    public LiveData<String> getPassword() {
+        return password;
+    }
 }
