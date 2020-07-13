b53296892608273f15372e38b9d81aba6bdf46eb
Selina Kim <selinakim@fb.com>
2020-07-13 13:53:43 -0700

    Finished Login Activity

--- app/src/main/java/com/example/nextstreet/login/LoginActivity.java
+++ app/src/main/java/com/example/nextstreet/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
@@ -15,6 +16,9 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.nextstreet.MainActivity;
 import com.example.nextstreet.R;
 import com.example.nextstreet.databinding.ActivityLoginBinding;
+import com.google.android.material.snackbar.Snackbar;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
@@ -37,16 +41,17 @@ public class LoginActivity extends AppCompatActivity {
         loginViewModel =
                 ViewModelProviders.of(this).get(LoginViewModel.class);
 
-        loginViewModel.getUsername().observe(this, new LoginActivity.textObserver(binding.etUsername));
-        loginViewModel.getPassword().observe(this, new LoginActivity.textObserver(binding.etPassword));
+        loginViewModel.getUsername().observe(this, new LoginActivity.TextObserver(binding.etUsername));
+        loginViewModel.getPassword().observe(this, new LoginActivity.TextObserver(binding.etPassword));
 
-        binding.btnSignup.setOnClickListener(new signupOnClickListener());
+        binding.btnLogin.setOnClickListener(new LoginOnClickListener());
+        binding.btnSignup.setOnClickListener(new SignupOnClickListener());
     }
 
-    private class textObserver implements Observer<String> {
+    private class TextObserver implements Observer<String> {
         TextView tv;
 
-        private textObserver(TextView tv) {
+        private TextObserver(TextView tv) {
             this.tv = tv;
         }
 
@@ -56,14 +61,45 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private class signupOnClickListener implements View.OnClickListener {
+    private class SignupOnClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
             Log.i(TAG, "signupOnClickListener onClick: ");
             goActivity(SignupActivity.class);
         }
     }
-    
+
+    private class LoginOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Log.i(TAG, "loginOnClickListener onClick: ");
+
+            loginUser(binding.etUsername.getText().toString(),
+                    binding.etPassword.getText().toString());
+        }
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "loginUser: " + username);
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    // TODO: State whether user has wrong username/password or not
+                    Log.e(TAG, "loginUser: issue with login", e);
+
+                    Snackbar.make(binding.getRoot(), R.string.toast_login_err,
+                            Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                goActivity(MainActivity.class);
+                Snackbar.make(binding.getRoot(), R.string.toast_login_succ,
+                        Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     private void goActivity(Class c) {
         Intent i = new Intent(this, c);
         startActivity(i);
