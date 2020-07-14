5399ad5f53f0a25e7f9319fe02412687ebaed4de
Selina Kim <selinakim@fb.com>
2020-07-14 09:17:19 -0700

    Set up CameraOnClickListener

--- app/src/main/java/com/example/nextstreet/listeners/CameraOnClickListener.java
+++ app/src/main/java/com/example/nextstreet/listeners/CameraOnClickListener.java
@@ -1,11 +1,41 @@
 package com.example.nextstreet.listeners;
 
+import android.app.Activity;
+import android.util.Log;
 import android.view.View;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.nextstreet.BitmapManipulation;
+
+import java.io.File;
+
 
 public class CameraOnClickListener implements View.OnClickListener{
 
+    private String TAG;
+    private Activity activity;
+    private Fragment fragment;
+    private String photoFileName;
+    private File photoFile;
+
+    public CameraOnClickListener(String TAG, Activity activity,
+                                 Fragment fragment, String photoFileName) {
+        this.TAG = TAG;
+        this.activity = activity;
+        this.fragment = fragment;
+        this.photoFileName = photoFileName;
+    }
+
     @Override
     public void onClick(View view) {
+        Log.i(TAG, "onClick: camera button was clicked by user");
+        photoFile =
+                BitmapManipulation.launchCamera(activity, fragment,
+                        photoFileName, TAG);
+    }
 
+    public File getPhotoFile() {
+        return photoFile;
     }
 }
