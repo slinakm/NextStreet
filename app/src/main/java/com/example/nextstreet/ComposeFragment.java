468034cdcde350fcd32506bd89614cfdcbdfd47b
Selina Kim <selinakim@fb.com>
2020-07-14 09:30:19 -0700

    Set up picture-taking  in Compose Fragment.

--- app/src/main/java/com/example/nextstreet/ComposeFragment.java
+++ app/src/main/java/com/example/nextstreet/ComposeFragment.java
@@ -1,9 +1,14 @@
 package com.example.nextstreet;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,16 +18,28 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.nextstreet.databinding.FragmentComposeBinding;
 import com.example.nextstreet.databinding.FragmentProfileBinding;
+import com.example.nextstreet.listeners.CameraOnClickListener;
 import com.example.nextstreet.listeners.DismissOnClickListener;
+import com.example.nextstreet.listeners.SnackbarOnClickListener;
 import com.example.nextstreet.listeners.TextObserver;
 import com.example.nextstreet.ui.trips.TripsViewModel;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ComposeFragment extends DialogFragment {
 
     private static final String TAG = ComposeFragment.class.getSimpleName();
+    private static final String photoFileName = "photo.jpg";
 
     private FragmentComposeBinding binding;
     private ComposeViewModel composeViewModel;
+
+    private CameraOnClickListener cameraOnClickListener;
+    private File photoFile;
 
     public static ComposeFragment newInstance() {
         Bundle args = new Bundle();
@@ -38,6 +55,11 @@ public class ComposeFragment extends DialogFragment {
                 ViewModelProviders.of(this).get(ComposeViewModel.class);
         binding = FragmentComposeBinding.inflate(getLayoutInflater());
         binding.ivCancel.setOnClickListener(new DismissOnClickListener(TAG, this));
+
+        cameraOnClickListener = new CameraOnClickListener(TAG, getActivity(),
+                this, photoFileName);
+        binding.btnCamera.setOnClickListener(cameraOnClickListener);
+
         return binding.getRoot();
     }
 
@@ -55,4 +77,32 @@ public class ComposeFragment extends DialogFragment {
         getDialog().getWindow().setLayout(ViewGroup.LayoutParams.FILL_PARENT,
                 ViewGroup.LayoutParams.FILL_PARENT);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == BitmapManipulation.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            File tempPhotoFile = cameraOnClickListener.getPhotoFile();
+
+            if (resultCode == RESULT_OK
+                    && tempPhotoFile != null) {
+                Bitmap takenImage = BitmapFactory.decodeFile(tempPhotoFile.getAbsolutePath());
+
+                Bitmap resizedBitmap = BitmapManipulation.scaleToFitWidth(takenImage,
+                        (int) getResources().getDimension((R.dimen.resized_post_image)));
+
+                binding.ivPackage.setImageBitmap(resizedBitmap);
+                binding.ivPackage.setVisibility(View.VISIBLE);
+
+                photoFile =
+                        BitmapManipulation.writeResizedBitmap(getContext(), photoFileName,
+                                resizedBitmap, "resized", TAG);
+            } else {
+                Snackbar.make(binding.getRoot(), getString(R.string.toast_camera_err),
+                        BaseTransientBottomBar.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
