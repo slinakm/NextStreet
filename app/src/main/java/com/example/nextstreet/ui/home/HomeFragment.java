404f9f50c5ec45183c5fb6b96bec83ca6b69b42e
Selina Kim <selinakim@fb.com>
2020-07-15 08:20:13 -0700

    Set up Google maps and Place Autocomplete ragment in XML file

--- app/src/main/java/com/example/nextstreet/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/nextstreet/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.nextstreet.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,24 +13,46 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.nextstreet.MainActivity;
 import com.example.nextstreet.R;
+import com.example.nextstreet.databinding.FragmentHomeBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment
+        implements OnMapReadyCallback {
 
+    private static final String TAG = HomeFragment.class.getSimpleName();
+
+    private FragmentHomeBinding binding;
     private HomeViewModel homeViewModel;
+    private GoogleMap mMap;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        binding = FragmentHomeBinding.inflate(getLayoutInflater());
+
+//        SupportMapFragment mapFragment = (SupportMapFragment) binding.map; //(SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+        SupportMapFragment mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        Log.i(TAG, "onCreateView: " + mMapFragment);
+        mMapFragment.getMapAsync(this);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
