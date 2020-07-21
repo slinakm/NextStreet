65408b4a46448f1fb81444a6445d3343f8342d17
Selina Kim <selinakim@fb.com>
2020-07-21 16:09:17 -0700

    Set up TripsAdapter'

--- app/src/main/java/com/example/nextstreet/ui/trips/TripsAdapter.java
+++ app/src/main/java/com/example/nextstreet/ui/trips/TripsAdapter.java
@@ -1,45 +1,90 @@
 package com.example.nextstreet.ui.trips;
 
-import android.view.View;
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.nextstreet.R;
+import com.example.nextstreet.databinding.ItemRequestBinding;
 import com.example.nextstreet.models.PackageRequest;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.common.base.Preconditions;
+import com.parse.ParseFile;
+import com.parse.ParseGeoPoint;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 public class TripsAdapter extends RecyclerView.Adapter<TripsAdapter.ViewHolder> {
 
-  private List<PackageRequest> packageRequests;
+  private static final String TAG = TripsAdapter.class.getSimpleName();
 
-  protected TripsAdapter(List<PackageRequest> packageRequests) {
+  private final Activity context;
+  private final List<PackageRequest> packageRequests;
+
+  protected TripsAdapter(Activity context, List<PackageRequest> packageRequests) {
+    this.context = context;
     this.packageRequests = packageRequests;
   }
 
-  @NonNull
   @Override
-  public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    return null;
+  public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    Log.i(TAG, "onCreateViewHolder: creating new viewholder");
+    ItemRequestBinding binding = ItemRequestBinding.inflate(context.getLayoutInflater(),
+            parent, false);
+    return new TripsAdapter.ViewHolder(binding);
   }
 
   @Override
-  public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+  public void onBindViewHolder(ViewHolder holder, int position) {
+    Log.i(TAG, "onBindViewHolder: binding");
     holder.bind(packageRequests.get(position));
   }
 
   @Override
   public int getItemCount() {
+    Log.i(TAG, "getItemCount");
     return packageRequests.size();
   }
 
   public class ViewHolder extends RecyclerView.ViewHolder {
 
-    public ViewHolder(@NonNull View itemView) {
-      super(itemView);
+    ItemRequestBinding binding;
+    public ViewHolder(ItemRequestBinding binding) {
+      super(binding.getRoot());
+
+      this.binding = binding;
     }
 
-    private void bind(PackageRequest request) {}
+    private void bind(PackageRequest request) {
+      binding.tvDescription.setText(request.getDescription());
+
+      ParseGeoPoint destination = request.getDestination();
+      ParseGeoPoint origin = request.getOrigin();
+      Preconditions.checkNotNull(destination, "Destination should not be null");
+      Preconditions.checkNotNull(origin, "Origin should not be null");
+
+      binding.tvOrigin.setText(new LatLng(origin.getLatitude(), origin.getLongitude()).toString());
+      binding.tvDestination.setText(new LatLng(destination.getLatitude(), destination.getLongitude()).toString());
+      binding.tvDistance.setText(
+          MessageFormat.format(
+              "{0} {1}",
+              (int) origin.distanceInMilesTo(destination),
+              context.getResources().getString(R.string.miles)));
+
+      ParseFile image = request.getImage();
+      if (image != null) {
+        Glide.with(context)
+                .load(image.getUrl())
+                .transform(new RoundedCorners(context.getResources()
+                        .getInteger(R.integer.rounded_corners)))
+                .into(binding.ivImage);
+      }
+    }
   }
 }
