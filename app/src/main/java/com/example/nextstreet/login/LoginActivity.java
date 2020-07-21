02bdf540fab2fb573dfc7ecb0d4d48a0dec0259a
Selina Kim <selinakim@fb.com>
2020-07-21 09:11:16 -0700

    Added LoginManager for Facebook Login

--- app/src/main/java/com/example/nextstreet/login/LoginActivity.java
+++ app/src/main/java/com/example/nextstreet/login/LoginActivity.java
@@ -9,81 +9,86 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.nextstreet.databinding.ActivityLoginBinding;
 import com.example.nextstreet.ui.MainActivity;
 import com.example.nextstreet.utilities.TextObserver;
-import com.example.nextstreet.databinding.ActivityLoginBinding;
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
+import com.facebook.login.LoginManager;
 import com.parse.ParseUser;
 
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static final String TAG = LoginActivity.class.getSimpleName();
-    private static final String EMAIL = "email";
+  private static final String TAG = LoginActivity.class.getSimpleName();
+  private static final String EMAIL = "email";
 
-    private ActivityLoginBinding binding;
+  private final CallbackManager callbackManager = CallbackManager.Factory.create();
 
-    private LoginViewModel loginViewModel;
-    private CallbackManager callbackManager;
+  private ActivityLoginBinding binding;
 
+  private LoginViewModel loginViewModel;
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    binding = ActivityLoginBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+
+    AccessToken accessToken = AccessToken.getCurrentAccessToken();
+    boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+    if (ParseUser.getCurrentUser() != null
+            || isLoggedIn) {
+      goActivity(MainActivity.class);
+    }
+
+    loginViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
+
+    loginViewModel.getUsername().observe(this, new TextObserver(binding.etUsername));
+    loginViewModel.getPassword().observe(this, new TextObserver(binding.etPassword));
+
+    binding.btnLogin.setOnClickListener(new LoginOnClickListener());
+    binding.btnSignup.setOnClickListener(new SignupOnClickListener());
+
+    binding.btnFb.setReadPermissions(Arrays.asList(EMAIL));
+
+    LoginManager.getInstance().registerCallback(callbackManager, new FBLoginCallback(this));
+  }
+
+  private class SignupOnClickListener implements View.OnClickListener {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        if (ParseUser.getCurrentUser() != null) {
-            goActivity(MainActivity.class);
-        }
-
-        loginViewModel =
-                ViewModelProviders.of(this).get(LoginViewModel.class);
-
-        loginViewModel.getUsername().observe(this, new TextObserver(binding.etUsername));
-        loginViewModel.getPassword().observe(this, new TextObserver(binding.etPassword));
-
-        binding.btnLogin.setOnClickListener(new LoginOnClickListener());
-        binding.btnSignup.setOnClickListener(new SignupOnClickListener());
-
-        callbackManager = CallbackManager.Factory.create();
-        binding.btnFb.setReadPermissions(Arrays.asList(EMAIL));
+    public void onClick(View view) {
+      Log.i(TAG, "signupOnClickListener onClick: ");
+      goActivity(SignupActivity.class);
     }
+  }
 
-
-    private class SignupOnClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-            Log.i(TAG, "signupOnClickListener onClick: ");
-            goActivity(SignupActivity.class);
-        }
-    }
-
-    private class LoginOnClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
-            Log.i(TAG, "loginOnClickListener onClick: ");
-
-            loginUser(binding.etUsername.getText().toString(),
-                    binding.etPassword.getText().toString());
-        }
-    }
-
-    private void loginUser(String username, String password) {
-        Log.i(TAG, "loginUser: " + username);
-
-        ParseUser.logInInBackground(username, password, new LoginCallback(TAG, binding.getRoot(), this) );
-    }
-
+  private class LoginOnClickListener implements View.OnClickListener {
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+    public void onClick(View view) {
+      Log.i(TAG, "loginOnClickListener onClick: ");
 
-    private void goActivity(Class c) {
-        Intent i = new Intent(this, c);
-        startActivity(i);
-        finish();
+      loginUser(binding.etUsername.getText().toString(), binding.etPassword.getText().toString());
     }
+  }
+
+  private void loginUser(String username, String password) {
+    Log.i(TAG, "loginUser: " + username);
+
+    ParseUser.logInInBackground(
+        username, password, new LoginCallback(TAG, binding.getRoot(), this));
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    callbackManager.onActivityResult(requestCode, resultCode, data);
+    super.onActivityResult(requestCode, resultCode, data);
+  }
+
+  private void goActivity(Class c) {
+    Intent i = new Intent(this, c);
+    startActivity(i);
+    finish();
+  }
 }
