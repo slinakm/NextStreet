edb241b8dca4e33ad566a987a70c2b8d363a9f18
Selina Kim <selinakim@fb.com>
2020-07-29 13:55:08 -0700

    Added a new menu icon drawable

--- app/src/test/java/com/example/nextstreet/ComposeFragmentTest.java
+++ app/src/test/java/com/example/nextstreet/ComposeFragmentTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
-public class ComposeFragmentCameraTest {
+public class ComposeFragmentTest {
 
 }
 
