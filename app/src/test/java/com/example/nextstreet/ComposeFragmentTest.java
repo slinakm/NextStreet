86908457ebd837033122d68cd9b5a876baee8324
Selina Kim <selinakim@fb.com>
2020-07-29 14:51:55 -0700

    Added Fragment Scenario to test

--- app/src/test/java/com/example/nextstreet/ComposeFragmentTest.java
+++ app/src/test/java/com/example/nextstreet/ComposeFragmentTest.java
@@ -1,15 +1,27 @@
 package com.example.nextstreet;
 
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.fragment.app.FragmentFactory;
+import androidx.fragment.app.testing.FragmentScenario;
+
 import com.example.nextstreet.compose.ComposeFragment;
 
 import org.apache.tools.ant.Main;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class ComposeFragmentTest {
+    @Test
+    public void testEventFragment() {
+        Bundle fragmentArgs = new Bundle();
 
+        FragmentFactory factory = new FragmentFactory();
+        FragmentScenario scenario
+                = FragmentScenario.launchInContainer(ComposeFragment.class, fragmentArgs, factory);
+    }
 }
 
